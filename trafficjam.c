52b175b37867706c5cd8f7a6345db54f1825b68c
Rayan <rayan.bensouiah@universite-paris-saclay.fr>
2025-12-27 14:31:04 +0100

    Restructuration

--- trafficjam.c
+++ trafficjam.c
@@ -3,101 +3,45 @@
 #include "console.h"
 #include <stdlib.h>
 
+
+/* INFORMATIONS SUR LA GRID */
 #define MAX_LENGTH 80
 #define MAX_HEIGHT 45
+#define GRID_SIZE 18
+#define CELL 3
+#define MAX_VEHICULES 10
+#define GRID_ORIGIN_X 32
+#define GRID_ORIGIN_Y 15
+
+/* COULEURS DE TEXTE (VEHICULES) */
+#define COLOR_BLACK 30
+#define COLOR_RED 31
+#define COLOR_GREEN 32
+#define COLOR_YELLOW 33
+#define COLOR_BLUE 34
+#define COLOR_PURPLE 35
+#define COLOR_CYAN 36
+#define COLOR_WHITE 37
 
 typedef struct {
-	int x;
-	int y;
-}Point;
-
-/* Fonction qui echange deux entiers en passage par pointeurs
- * @input : (int) x, (int) y
- * @return : NONE */
-void swap(int *X, int *Y)
-{
-        int temp = *X; //save X
-        *X = *Y; //cp Y in X
-        *Y = temp; //cp saved X in Y
-}
+	int id; // référencer les véhicules de la grille
+	int x, y; // top-left point vehicule
+	char dir; // 'h'(horizontal) or 'v' (vertical)
+	int length; // 2(car) or 3(truck)
+	int color; //differencier la main car coté UX 
+}Vehicule;
 
 
-
-/* Fonction affichant un plateau vide
- * @input : (int) Longueur souhaitée
- * @return : affichage */
-void generategrid(int gridl)
-{
-	/* Clear View */
-	resetterminal();
-	flushstdin();
-	desactive_curseur();
-	
-	/* Espace libre (pour les informations) */
-	int xspace = MAX_LENGTH - gridl; //espace (Left/Right) de notre fenetre de jeu
-	int yspace = MAX_HEIGHT - gridl; //espace (Top/Bottom) de notre fenetre de jeu
-	
-	/* Fenetre de jeu */
-	Point ptl = {31, 13}; //TopLeft
-	Point ptr = {50, 13}; //TopRight
-	Point pbl = {31, 31}; //BottomLeft
-	Point pbr = {50, 31}; //BottomRight
-
-	
-
-	efface_ligne();
-	allerxy(ptl.x, ptl.y);   	
-	printf("+%.*s+", gridl, "==============================="); 
-
-	for(int i = 1; i <= gridl; i++){
-	
-		if(i == ((pbl.y - ptl.y) / 2)){
-			allerxy(ptl.x, (ptl.y + ptl.x)/2);
-		       	printf("|%.*s>", gridl, "...............................");
-		}
-		else {
-			allerxy(ptl.x, ptl.y + i);
-			printf("|%.*s|", gridl, "...............................");
-		}
-	}
-	allerxy(pbl.x, pbl.y);
-	printf("+%.*s+", gridl, "==============================");
-	//if (recupcar() == 'q') resetterminal(); // reset si l'utilisateur appuie sur q (énoncé)
-}
-
-/* Fonction affichant un véhicule rouge
- * @input : (Point) Rc{x,y} TopLeft de la voiture
- * 	    (int) hv horizontal(0) or Vertical(1) 
- * @return : affichage */
-
-void generatemainCar(Point Rc, char direction, int color)
-{
-	int carlength = 6;
-	int carheight = 3;
-	
-	if(direction != 'h') {
-		swap(&carlength, &carheight);
-	}
-	
-	couleurfond(color);
-	//couleurpolice(31); // 30 Black, 31 Red, 32 Green, 33 Yellow, 34 Blue, 35 Magenta, 36 Cyan, 37 White
-	// allerxy(Rc.x, Rc.y);
-	for(int i = 0; i < carheight; i++){
-			allerxy(Rc.x, Rc.y + i);
-			printf("%.*s", carlength, "                     ");			
-	}
-	if (recupcar() == 'q') resetterminal(); // reset si l'utilisateur appuie sur q (énoncé)
-	
-}
+typedef struct{
+	Vehicule cars[MAX_VEHICULES]; //vehicules de la grille (array)
+	int count;
+	int selected;
+}Game;
 
 int main(void)
 {
-	Point start = {32, 15};
-	Point p2 = {40, 15};
-        generategrid(18);
-	generatemainCar(start, 'h', 31);
-	generatemainCar(p2, 'v', 32);
-	generatemainCar(zero, 'h', 33);
-	
-	generatemainCar(end, 'h', 35);
+	printf("Memory occupation :\n");
+	printf("sizeof(Vehicle)  = %lu bytes\n", sizeof(Vehicule));
+    	printf("sizeof(GameState)= %lu bytes\n", sizeof(Game));
+	return 0;	
 }
